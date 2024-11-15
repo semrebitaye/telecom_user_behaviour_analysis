f3f3f48364e28d481d61b2a0325e363897c1f2ee
semrebitaye <asimrobitaye23@gmail.com>
2024-11-15 21:16:44 +0300

    source

--- src/utils.py
+++ src/utils.py
@@ -1,52 +1,67 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import zscore 
+import scipy.stats 
+from scipy.stats import zscore
 
 def missing_values_table(df):
-    # total missing values
+    # Total missing values
     mis_val = df.isnull().sum()
 
-    # percentage of missing value
+    # Percentage of missing values
     mis_val_percent = 100 * df.isnull().sum() / len(df)
- 
-    # dtype of missing value
-    mis_val_type = df.dtypes
 
-    # make a table with the results
-    mis_val_table = pd.concat([mis_val, mis_val_percent, mis_val_type], axis=1)
+    # dtype of missing values
+    mis_val_dtype = df.dtypes
 
-    # rename the columns
-    mis_value_table_ren_columns = mis_val_table.rename(columns={0: 'mis_val', 1: '% of Total Values', 2: 'Dtype'})
+    # Make a table with the results
+    mis_val_table = pd.concat([mis_val, mis_val_percent, mis_val_dtype], axis=1)
 
-    # sort the table by percentage of missing descending
-    mis_value_table_ren_columns = mis_value_table_ren_columns[mis_value_table_ren_columns.iloc[:, 1] != 0].sort_values('% of Total Values', ascending=False).round(1)   
- 
-    # print some summary information
-    print("Your selected dataframe has " + str(df.shape[1]) + " columns\n"
-        "There are " + str(mis_value_table_ren_columns.shape[0]) + " columns that have missing values.")
+    # Rename the columns
+    mis_val_table_ren_columns = mis_val_table.rename(
+    columns={0: 'Missing Values', 1: '% of Total Values', 2: 'Dtype'})
 
-    # return the dataframe with missing information
-    return mis_value_table_ren_columns
+    # Sort the table by percentage of missing descending
+    mis_val_table_ren_columns = mis_val_table_ren_columns[
+        mis_val_table_ren_columns.iloc[:, 1] != 0].sort_values(
+        '% of Total Values', ascending=False).round(1)
+
+    # Print some summary information
+    print("Your selected dataframe has " + str(df.shape[1]) + " columns.\n"
+          "There are " + str(mis_val_table_ren_columns.shape[0]) +
+          " columns that have missing values.")
+
+    # Return the dataframe with missing information
+    return mis_val_table_ren_columns
 
 
-def connvert_bytes_to_megabytes(df, bytes_data):
-    megabytes = 1 * 10**6 
-    df[bytes_data] = df[bytes_data] / megabytes
+def format_float(value):
+    return f'{value:,.2f}'
+
+
+def find_agg(df: pd.DataFrame, agg_column: str, agg_metric: str, col_name: str, top: int, order=False) -> pd.DataFrame:
+    new_df = df.groupby(agg_column)[agg_column].agg(agg_metric).reset_index(name=col_name). \
+        sort_values(by=col_name, ascending=order)[:top]
+    return new_df
+
+
+def convert_bytes_to_megabytes(df, bytes_data):
+    megabyte = 1 * 10e+5
+    df[bytes_data] = df[bytes_data] / megabyte
     return df[bytes_data]
 
 
 def fix_outlier(df, column):
     df[column] = np.where(df[column] > df[column].quantile(0.95), df[column].median(), df[column])
-    return 
+    return df[column]
 
 def remove_outliers(df, column_to_process, z_threshold=3):
-    # apply outlier removal to the sortified column
+    # Apply outlier removal to the specified column
     z_scores = zscore(df[column_to_process])
-    outlier_column = column_to_process + "_Outlier"
+    outlier_column = column_to_process + '_Outlier'
     df[outlier_column] = (np.abs(z_scores) > z_threshold).astype(int)
-    df = df[df[outlier_column] == 0]
+    df = df[df[outlier_column] == 0]  # Keep rows without outliers
 
-    # drop the outlier column
+    # Drop the outlier column as it's no longer needed
     df = df.drop(columns=[outlier_column], errors='ignore')
 
     return df
